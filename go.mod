dc31de90cb6eaacaa3398e10e096eb514a96d252
Christian Engel <christian.engel@schindler.com>
2021-10-05 19:42:47 +0200

    Move from Makefiles to one build-script. Further scripts for patch, minor, major bumpversion calls. distclean added as a further script.

--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
 module miniWebSrv
 
-go 1.14
+go 1.17
 
-require github.com/urfave/cli v1.22.4
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/urfave/cli v1.22.5
+)
+
+require github.com/russross/blackfriday/v2 v2.1.0 // indirect
--- version.sh
+++ version.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
+# -d as debug option to show if the correct line is identified
+# without -d, the line is reduced to the pure version number.
+
 appDir=$(dirname $0)
 cd "$appDir"
 
-START="egrep -ih 'app(\.)?Version[[:space:]]*=' *.go | egrep '[0-9]+\.[0-9]+\.[0-9]+' | tail -n1"
+START="egrep -i$_egrepFlag 'app(\.)?Version[[:space:]]*=' *.go | egrep '[0-9]+\.[0-9]+\.[0-9]+' | tail -n1"
 
 if [ "$1" = -d ] ; then
+   _egrepFlag=-H
    eval $START
 else
+   _egrepFlag=-h
    appVersion=$(eval $START | sed 's/^.*=//' | sed 's/\"//g' | sed 's,//.*,,' | sed 's/[[:space:]]//g' | sed 's/^-//')
-   [ -z "$appVersion" ] && echo could not determine app version && exit 1
+   [ -z "$appVersion" ] && echo could not determine app version 1>&2 && exit 1
    echo $appVersion
 fi
